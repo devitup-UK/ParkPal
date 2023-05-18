47d3a23e587d462d7a268adc84dbc545a95e87cd
Tom Alderson <tom.alderson@devitup.co.uk>
2023-05-18 17:18:04 +0100

    Updated ParkPal website to include links to Play store.

--- ParkPal.Website/src/main.ts
+++ ParkPal.Website/src/main.ts
@@ -6,7 +6,7 @@ import Ads from 'vue-google-adsense'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faClock,  faSearch, faBell, faHeart, faPaintRoller, faAppleWhole, faAt, faCode, faPhotoFilm, faStar, faPhone, faLaptopCode, faMobileAlt, faPaintBrush, faMapMarkerAlt, faEnvelope, faShareAlt, faTimes, faExclamationCircle, faMobile } from '@fortawesome/free-solid-svg-icons'
-import { faApple, faFacebook, faTwitter, faInstagram, faPinterest } from '@fortawesome/free-brands-svg-icons'
+import { faApple, faFacebook, faTwitter, faInstagram, faPinterest, faAndroid } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 import Adsense from "vue-google-adsense/dist/ads/Adsense";
 
@@ -16,7 +16,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
 Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
-library.add(faSearch, faBell, faHeart, faClock, faPaintRoller, faApple, faAt, faPhotoFilm, faStar, faMobile, faCode, faPhone, faFacebook, faTwitter, faInstagram, faPinterest, faLaptopCode, faMobileAlt, faPaintBrush, faMapMarkerAlt, faEnvelope, faShareAlt, faTimes, faExclamationCircle)
+library.add(faAndroid, faSearch, faBell, faHeart, faClock, faPaintRoller, faApple, faAt, faPhotoFilm, faStar, faMobile, faCode, faPhone, faFacebook, faTwitter, faInstagram, faPinterest, faLaptopCode, faMobileAlt, faPaintBrush, faMapMarkerAlt, faEnvelope, faShareAlt, faTimes, faExclamationCircle)
 
 Vue.use(BootstrapVue);
 // eslint-disable-next-line
